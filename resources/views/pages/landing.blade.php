ee9b527e1a614b68324d36a14dbf0b9301660346
DrMaxis <Grandmasterantwii@gmail.com>
2018-09-09 06:56:58 -0400

    jjp

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.ui')
+
+
+
+
+<div class="jj-hero">
+  
+    </div>
+
+
+
+    <div id="jj-partners">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 text-center">
+                        <div class="feature-center animate-box" data-animate-effect="fadeIn">
+                            <span class="icon">
+                                <i class="jj-i amazon">
+
+                                </i>
+                            </span>
+                            <h3>Amazon</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 text-center">
+                        <div class="feature-center animate-box" data-animate-effect="fadeIn">
+                            <span class="icon">
+                                <i class="jj-i wayfair"></i>
+                            </span>
+                            <h3>WayFair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 text-center">
+                        <div class="feature-center animate-box" data-animate-effect="fadeIn">
+                            <span class="icon">
+                                <i class="jj-i homedepot"></i>
+                            </span>
+                            <h3>Homedepot</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 text-center">
+                            <div class="feature-center animate-box" data-animate-effect="fadeIn">
+                                <span class="icon">
+                                    <i class="jj-i walmart "></i>
+                                </span>
+                                <h3>Walmart</h3>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    
